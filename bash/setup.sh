bb2493defd2fcd8cf42ff1a0f05cbb4bebad7530
George Adams IV <g.w.adams.iv@gmail.com>
2011-12-09 21:42:59 -0500

    why was i listing every file?

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname $0)
 
-cp $BASEDIR/bashrc ~/.bashrc
-cp $BASEDIR/bash_aliases ~/.bash_aliases
-cp $BASEDIR/bash_functions ~/.bash_functions
-cp $BASEDIR/bash_logout ~/.bash_logout
+for f in $(ls $BASEDIR)
+do
+    cp $BASEDIR/$f ~/.$f
+done
